6639cfd29dea12997469f2991cb218371e0250fe
Bruno Mariz <bmarizot@gmail.com>
2024-03-01 15:39:47 -0300

    create generate args and C program skeleton

---
+++ examples/acoustic_2D_generate_args.py
@@ -0,0 +1,227 @@
+from simwave import (
+    SpaceModel, TimeModel, RickerWavelet, Solver, Compiler,
+    Receiver, Source, plot_wavefield, plot_shotrecord, plot_velocity_model
+)
+import numpy as np
+
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+
+
+
+# available language options:
+# c (sequential)
+# cpu_openmp (parallel CPU)
+# gpu_openmp (GPU)
+# gpu_openacc (GPU)
+compiler_options = {
+    'c': {
+        'cc': 'gcc',
+        'language': 'c',
+        'cflags': '-O3 -fPIC -ffast-math -Wall -std=c99 -shared'
+    },
+    'cpu_openmp': {
+        'cc': 'gcc',
+        'language': 'cpu_openmp',
+        'cflags': '-O3 -fPIC -ffast-math -Wall -std=c99 -shared -fopenmp'
+    },
+    'gpu_openmp': {
+        'cc': 'clang',
+        'language': 'gpu_openmp',
+        'cflags': '-O3 -fPIC -ffast-math -fopenmp \
+                   -fopenmp-targets=nvptx64-nvidia-cuda \
+                   -Xopenmp-target -march=sm_75'
+    },
+    'gpu_openacc': {
+        'cc': 'pgcc',
+        'language': 'gpu_openacc',
+        'cflags': '-O3 -fPIC -acc:gpu -gpu=pinned -mp'
+    },
+}
+
+selected_compiler = compiler_options['c']
+
+# set compiler options
+compiler = Compiler(
+    cc=selected_compiler['cc'],
+    language=selected_compiler['language'],
+    cflags=selected_compiler['cflags']
+)
+
+# Velocity model
+vel = np.zeros(shape=(512, 512), dtype=np.float32)
+vel[:] = 1500.0
+vel[100:] = 2000.0
+
+# create the space model
+space_model = SpaceModel(
+    bounding_box=(0, 5120, 0, 5120),
+    grid_spacing=(10, 10),
+    velocity_model=vel,
+    space_order=4,
+    dtype=np.float32
+)
+
+# config boundary conditions
+# (none,  null_dirichlet or null_neumann)
+space_model.config_boundary(
+    damping_length=0,
+    boundary_condition=(
+        "null_neumann", "null_dirichlet",
+        "none", "null_dirichlet"
+    ),
+    damping_polynomial_degree=3,
+    damping_alpha=0.001
+)
+
+# create the time model
+time_model = TimeModel(
+    space_model=space_model,
+    tf=1.0,
+    saving_stride=0
+)
+
+# create the set of sources
+source = Source(
+    space_model,
+    coordinates=[(2560, 2560)],
+    window_radius=4
+)
+
+# crete the set of receivers
+receiver = Receiver(
+    space_model=space_model,
+    coordinates=[(2560, i) for i in range(0, 5120, 10)],
+    window_radius=4
+)
+
+# create a ricker wavelet with 10hz of peak frequency
+ricker = RickerWavelet(10.0, time_model)
+
+# create the solver
+solver = Solver(
+    space_model=space_model,
+    time_model=time_model,
+    sources=source,
+    receivers=receiver,
+    wavelet=ricker,
+    compiler=compiler
+)
+
+print("Timesteps:", time_model.timesteps)
+
+# generate args
+args = {
+    "solver.u_full":{
+        "name":"u_full",
+        "value":solver.u_full},
+    "solver.space_model.extended_velocity_model":{
+        "name":"velocity_model",
+        "value":solver.space_model.extended_velocity_model},
+    "solver.space_model.extended_density_model":{
+        "name":"density_model",
+        "value":solver.space_model.extended_density_model},
+    "solver.space_model.damping_mask":{
+        "name":"damping_mask",
+        "value":solver.space_model.damping_mask},
+    "solver.wavelet.values":{
+        "name":"wavelet",
+        "value":solver.wavelet.values},
+    "solver.wavelet.timesteps":{
+        "name":"wavelet_size",
+        "value":solver.wavelet.timesteps},
+    "solver.wavelet.num_sources":{
+        "name":"wavelet_count",
+        "value":solver.wavelet.num_sources},
+    "solver.space_model.fd_coefficients(2)":{
+        "name":"second_order_fd_coefficients",
+        "value":solver.space_model.fd_coefficients(2)},
+    "solver.space_model.fd_coefficients(1)":{
+        "name":"first_order_fd_coefficients",
+        "value":solver.space_model.fd_coefficients(1)},
+    "solver.space_model.boundary_condition":{
+        "name":"boundary_condition",
+        "value":solver.space_model.boundary_condition},
+    "solver.sources.interpolated_points_and_values[0]":{
+        "name":"src_points_interval",
+        "value":solver.sources.interpolated_points_and_values[0]},
+    "len(solver.sources.interpolated_points_and_values[0])":{
+        "name":"src_points_interval_size",
+        "value":len(solver.sources.interpolated_points_and_values[0])},
+    "solver.sources.interpolated_points_and_values[1]":{
+        "name":"src_points_values",
+        "value":solver.sources.interpolated_points_and_values[1]},
+    "solver.sources.interpolated_points_and_values[2]":{
+        "name":"src_points_values_offset",
+        "value":solver.sources.interpolated_points_and_values[2]},
+    "len(solver.sources.interpolated_points_and_values[1])":{
+        "name":"src_points_values_size",
+        "value":len(solver.sources.interpolated_points_and_values[1])},
+    "solver.receivers.interpolated_points_and_values[0]":{
+        "name":"rec_points_interval",
+        "value":solver.receivers.interpolated_points_and_values[0]},
+    "len(solver.receivers.interpolated_points_and_values[0])":{
+        "name":"rec_points_interval_size",
+        "value":len(solver.receivers.interpolated_points_and_values[0])},
+    "solver.receivers.interpolated_points_and_values[1]":{
+        "name":"rec_points_values",
+        "value":solver.receivers.interpolated_points_and_values[1]},
+    "solver.receivers.interpolated_points_and_values[2]":{
+        "name":"rec_points_values_offset",
+        "value":solver.receivers.interpolated_points_and_values[2]},
+    "len(solver.receivers.interpolated_points_and_values[1])":{
+        "name":"rec_points_values_size",
+        "value":len(solver.receivers.interpolated_points_and_values[1])},
+    "solver.shot_record":{
+        "name":"shot_record",
+        "value":solver.shot_record},
+    "solver.sources.count":{
+        "name":"num_sources",
+        "value":solver.sources.count},
+    "solver.receivers.count":{
+        "name":"num_receivers",
+        "value":solver.receivers.count},
+    "solver.space_model.grid_spacing":{
+        "name":"grid_spacing",
+        "value":solver.space_model.grid_spacing},
+    "solver.time_model.saving_stride":{
+        "name":"saving_stride",
+        "value":solver.time_model.saving_stride},
+    "solver.time_model.dt":{
+        "name":"dt",
+        "value":solver.time_model.dt},
+    "1":{
+        "name":"begin_timestep",
+        "value":1},
+    "solver.time_model.timesteps":{
+        "name":"end_timestep",
+        "value":solver.time_model.timesteps},
+    "solver.space_model.space_order":{
+        "name":"space_order",
+        "value":solver.space_model.space_order},
+    "solver.u_full.shape[0]":{
+        "name":"num_snapshots",
+        "value":solver.u_full.shape[0]}
+}
+
+import json
+for arg in args:
+    with open(f"args/{args[arg]['name']}", "w+") as f:
+        value = args[arg]["value"]
+        try:
+            value = json.dumps(value)
+        except TypeError:
+            if type(value)=="ndarray":
+                value = json.dumps(value.tolist())
+            elif type(value) == "tuple":
+                value = json.dumps(list(value))
+        f.write(str(value))
+        print(args[arg]["name"])
+
+# run the forward
+u_full, recv = solver.forward()
+
+print("u_full shape:", u_full.shape)
+plot_velocity_model(space_model.velocity_model)
+plot_wavefield(u_full[-1])
+plot_shotrecord(recv)
---
+++ examples/c/acoustic_2D.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+#include "../../simwave/kernel/backend/c_code/forward/constant_density/2d/wave.h"
+
+#define U_SIZE 300
+#define VELOCITY_SIZE 300
+#define DAMP_SIZE 300
+#define WAVELET_SIZE 300
+#define COEFF_SIZE 300
+#define BOUNDARY_SIZE 300
+#define SRC_POINTS_INTERVAL_SIZE 300
+#define SRC_POINTS_VALUES_SIZE 300
+
+int main() {
+    // Declare variables
+    f_type *u;
+    f_type *velocity;
+    f_type *damp;
+    f_type *wavelet;
+    size_t wavelet_size;
+    size_t wavelet_count;
+    f_type *coeff;
+    size_t *boundary_conditions;
+    size_t *src_points_interval;
+    size_t src_points_interval_size;
+    f_type *src_points_values;
+    size_t src_points_values_size;
+    size_t *src_points_values_offset;
+    size_t *rec_points_interval;
+    size_t rec_points_interval_size;
+    f_type *rec_points_values;
+    size_t rec_points_values_size;
+    size_t *rec_points_values_offset;
+    f_type *receivers;
+    size_t num_sources;
+    size_t num_receivers;
+    size_t nz;
+    size_t nx;
+    f_type dz;
+    f_type dx;
+    size_t saving_stride;
+    f_type dt;
+    size_t begin_timestep;
+    size_t end_timestep;
+    size_t space_order;
+    size_t num_snapshots;
+
+    // TODO: populate variables
+
+    // Call forward simulation
+    forward(u, velocity, damp, wavelet, wavelet_size, wavelet_count, coeff,
+            boundary_conditions, src_points_interval, src_points_interval_size,
+            src_points_values, src_points_values_size, src_points_values_offset,
+            rec_points_interval, rec_points_interval_size, rec_points_values,
+            rec_points_values_size, rec_points_values_offset, receivers,
+            num_sources, num_receivers, nz, nx, dz, dx, saving_stride, dt,
+            begin_timestep, end_timestep, space_order, num_snapshots);
+
+    return 0;
+}

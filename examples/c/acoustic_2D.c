61987419377fc03ac2a4d93ce5f8b166f9fe67f8
Bruno Mariz <bmarizot@gmail.com>
2024-03-02 21:10:34 -0300

    populate first variables

--- examples/c/acoustic_2D.c
+++ examples/c/acoustic_2D.c
@@ -3,7 +3,7 @@
 #include "../../simwave/kernel/backend/c_code/forward/constant_density/2d/wave.h"
 
 int main() {
-    // Declare variables
+    // ## Declare variables
     f_type *u;
     f_type *velocity;
     f_type *damp;
@@ -36,9 +36,33 @@ int main() {
     size_t space_order;
     size_t num_snapshots;
 
-    // TODO: populate variables
+    // ## Populate Variables
 
-    // Call forward simulation
+    // ### u
+    const int u_x_size = 3;
+    const int u_y_size = 517;
+    const int u_z_size = 517;
+    u = malloc(sizeof(f_type) * u_x_size * u_y_size * u_z_size);
+    for (size_t x = 0; x < u_x_size; x++) {
+        for (size_t y = 0; y < u_y_size; y++) {
+            for (size_t z = 0; z < u_z_size; z++) {
+                u[x * u_y_size * u_z_size + y * u_z_size + z] = (f_type)0;
+            }
+        }
+    }
+
+    // ## velocity
+    const int velocity_x_size = 517;
+    const int velocity_z_size = 517;
+    velocity = malloc(sizeof(f_type) * velocity_x_size * velocity_z_size);
+    for (size_t x = 0; x < velocity_x_size; x++) {
+        for (size_t z = 0; z < velocity_z_size; z++) {
+            velocity[x * velocity_z_size + z] =
+                x > velocity_x_size ? 1500 : 2000;
+        }
+    }
+
+    // ## Call forward simulation
     forward(u, velocity, damp, wavelet, wavelet_size, wavelet_count, coeff,
             boundary_conditions, src_points_interval, src_points_interval_size,
             src_points_values, src_points_values_size, src_points_values_offset,
